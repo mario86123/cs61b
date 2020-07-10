c9ccc7ffe7f2befc322071ce5652aec7c8422d1b
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2020-07-10 14:07:15 +0800

    complete readBodies method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -4,4 +4,22 @@ public class NBody {
 		int num = in.readInt();
 		return in.readDouble();
 	}
+	public static Body[] readBodies(String file) {
+		In in = new In(file);
+		int num = in.readInt();
+		double rad = in.readDouble();
+		Body[] arr = new Body[num];
+		for (int k = 0; k < num; k += 1) {
+			double xp, yp, xv, yv, m;
+			String img;
+			xp = in.readDouble();
+			yp = in.readDouble();
+			xv = in.readDouble();
+			yv = in.readDouble();
+			m = in.readDouble();
+			img = in.readString();
+			arr[k] = new Body(xp, yp, xv, yv, m, img);
+		}
+		return arr;
+	}
 }
