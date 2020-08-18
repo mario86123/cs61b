2fdc9350da38048a1e38573c180715a6718928ab
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2020-08-18 22:27:46 +0800

    have not finished clab6 yet

--- clab/clab6/BubbleGrid.java
+++ clab/clab6/BubbleGrid.java
@@ -8,12 +8,40 @@ public class BubbleGrid {
         this.grid = grid;
     }
 
+    private int topUnionSize(int tmp[][]) {
+
+        return ;
+    }
+
     /* Returns an array whose i-th element is the number of bubbles that
      * fall after the i-th dart is thrown. Assume all elements of darts
      * are unique, valid locations in the grid. Must be non-destructive
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
         // TODO
-        return null;
+        int numOfRows = grid.length;
+        int numOfCols = grid[0].length;
+        int numOfDarts = darts.length;
+        int[] ans = new int[numOfDarts];
+
+        UnionFind uf = new UnionFind(numOfRows * numOfCols);
+
+        for (int i = 0; i < numOfDarts; i+=1) {
+            if (grid[darts[i][0]][darts[i][1]] == 0) {
+                ans[i] = 0;
+            } else {
+                int[][] afterDart = new int[numOfRows][numOfCols];
+                for (int j = 0; j < numOfRows; j += 1) {
+                    for (int k = 0; k < numOfCols; k += 1) {
+                        afterDart[j][k] = grid[j][k];
+                    }
+                }
+                afterDart[darts[i][0]][darts[i][1]] = 0;
+
+                ans[i] = topUnionSize(grid) - topUnionSize(afterDart);
+                // wrong thought, maybe dart is also in the top union
+            }
+        }
+        return ans;
     }
 }
--- clab/clab6/BubbleGridTest.java
+++ clab/clab6/BubbleGridTest.java
@@ -12,6 +12,20 @@ public class BubbleGridTest {
         int[] expected = {2};
 
         validate(grid, darts, expected);
+
+        int[][] grid1 = {{1, 1, 0},
+                         {1, 0, 0},
+                         {1, 1, 0},
+                         {1, 1, 1}};
+        int[][] darts1 = {{1, 0}};
+        int[] expected1 = {5};
+
+        validate(grid1, darts1, expected1);
+
+        int[][] darts2 = {{2, 2}, {2, 0}};
+        int[] expected2 = {0, 4};
+
+        validate(grid1, darts2, expected2);
     }
 
     private void validate(int[][] grid, int[][] darts, int[] expected) {
