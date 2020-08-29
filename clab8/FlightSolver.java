777d9cfe9ab935c30e7bad9789b0089a2d70e4e7
mario86123 <mario86123.eed05@g2.nctu.edu.tw>
2020-08-30 00:46:29 +0800

    finished clab8 problem 1

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,31 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
-
+    /* FIX ME */
+    ArrayList<Flight> f;
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        this.f = flights;
     }
 
     public int solve() {
         /* FIX ME */
-        return -1;
-    }
+        PriorityQueue<Flight> flightPQ = new PriorityQueue<>((i, j) -> i.endTime - j.endTime);
+        int maxTotalPassengers = 0, currentTotalPassengers;
 
+        flightPQ.addAll(f);
+
+        while (flightPQ.size() != 0) {
+            Flight minEndTimeFlight = flightPQ.poll();
+            currentTotalPassengers = minEndTimeFlight.passengers;
+            for (Flight tmp:flightPQ) {
+                if (tmp.startTime < minEndTimeFlight.endTime) {
+                    currentTotalPassengers += tmp.passengers;
+                }
+            }
+            if (currentTotalPassengers > maxTotalPassengers) {
+                maxTotalPassengers = currentTotalPassengers;
+            }
+        }
+        return maxTotalPassengers;
+    }
 }
